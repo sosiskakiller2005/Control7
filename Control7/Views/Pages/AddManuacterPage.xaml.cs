d9f40268cb7e251992aac9683afeb9a49da351b7
Козлов.Владислав <Vkozlov15012005@gmail.com>
2024-11-15 10:37:14 +0300

    Добавьте файлы проекта.

---
+++ Control7/Views/Pages/AddManuacterPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Control7.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Control7.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddManuacterPage.xaml
+    /// </summary>
+    public partial class AddManuacterPage : Page
+    {
+        private static Entities _context = new Entities();
+        public AddManuacterPage()
+        {
+            InitializeComponent();
+            ManufacterCmb.ItemsSource = _context.Manufatcer.ToList();
+            ManufacterCmb.DisplayMemberPath = "Name";
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (ManufacterCmb.SelectedItem != null && MaterialNameTb.Text != string.Empty)
+            {
+                Material newMaterial = new Material()
+                {
+                    Name = MaterialNameTb.Text,
+                    Manufatcer = ManufacterCmb.SelectedItem as Manufatcer
+                };
+                _context.Material.Add(newMaterial);
+                _context.SaveChanges();
+                MessageBox.Show("Материал добавлен.");
+            }
+        }
+    }
+}
---
+++ Control7/Views/Windows/MenuWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Control7.AppData;
+using Control7.Views.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Control7.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для MenuWindow.xaml
+    /// </summary>
+    public partial class MenuWindow : Window
+    {
+        public MenuWindow()
+        {
+            InitializeComponent();
+            FrameHelper.selectedFrame = MainFrm;
+            MainFrm.Navigate(new StartPage());
+        }
+
+        private void AddManufacterBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddMaterialBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddEmployee_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AccountingBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OtchetBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
